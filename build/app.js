e9b3faff9a886989d51ada0914025b33c0b86384
Camden Rudisill <camden.rudisill@richmond.edu>
2015-08-15 17:50:15 -0400

    Update: rebuild with concat

--- build/app.js
+++
@@ -1,1 +0,0 @@
-window.onload=function(){var a=new Phaser.Game(800,600,Phaser.AUTO,"gameContainer");a.state.add("Boot",BasicGame.Boot),a.state.add("Preloader",BasicGame.Preloader),a.state.add("MainMenu",BasicGame.MainMenu),a.state.add("Game",BasicGame.Game),a.state.start("Boot")};
